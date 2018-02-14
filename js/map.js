8e64d586bc3af5f896da374f1d2d37b5483a0eb4
remimichel <remi.michel38@gmail.com>
2018-02-14 22:42:34 +0100

    fix(Reservation): alert duplications

--- js/map.js
+++ js/map.js
@@ -2,6 +2,8 @@ class App {
   constructor() {
     this.initMap();
     this.loadApi();
+
+    this.initReservationListener()
   }
 
   //---------- Initialisation google map -----------
@@ -19,10 +21,12 @@ class App {
   //---------- Récupération données stations -----------
   //---------------------------------------------------
   loadApi() {
-    $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=00725f1585ae004d2043b59894843d43b6650b8e").then(api => {
-      this.api = api;
-      this.initMarkers();
-    });
+    $.getJSON("https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=00725f1585ae004d2043b59894843d43b6650b8e")
+      .then(api => {
+        this.api = api;
+        this.initMarkers();
+      })
+    ;
   }
 
   //---------- initialisation des markers -----------
@@ -30,7 +34,7 @@ class App {
   initMarkers() {
     let markers = [];
     for (let i = 0; i < this.api.length; i++) {
-      let station = this.api[i];
+      const station = this.api[i];
       const iconMarker = {
         url: "js/images/marker-green.png",
         origin: new google.maps.Point(0, 0),
@@ -52,7 +56,7 @@ class App {
       markers = markers.concat(this.marker);
       this.infoStation(station);
     }
-    this.markerCluster = new MarkerClusterer(veloStation.map, markers, {
+    new MarkerClusterer(veloStation.map, markers, {
       imagePath:
         "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m"
     });
@@ -61,7 +65,7 @@ class App {
   //---------- Block informations station -----------
   //---------------------------------------------------
   infoStation(station) {
-    google.maps.event.addListener(this.marker,"click", () => {
+    google.maps.event.addListener(this.marker, "click", () => {
       this.station = station;
       console.log(this.station);
       const reservation = document.querySelector("#reservation");
@@ -69,55 +73,56 @@ class App {
       const confirmation = document.querySelector("#confirmation");
       const infos = document.querySelector("#informations");
 
-        reservation.style.display = "block";
-        reserver.style.display = "block";
-        confirmation.style.display = "none";
-        reservation.scrollIntoView();
+      reservation.style.display = "block";
+      reserver.style.display = "block";
+      confirmation.style.display = "none";
+      reservation.scrollIntoView();
 
-        if (station.status === "OPEN") {
-          station.status = "OUVERTE";
-        } else if (station.status === "CLOSE") {
-          station.status = "FERMÉE";
-        }
+      if (station.status === "OPEN") {
+        station.status = "OUVERTE";
+      } else if (station.status === "CLOSE") {
+        station.status = "FERMÉE";
+      }
 
-        infos.innerHTML = `
+      infos.innerHTML = `
           <p>Adresse : <span>${station.address}</span><p>
           <p>État de la station: <span>${station.status}</span></p>
           <p>Vélos disponibles: <span>${station.available_bikes}</span></p>
           <p>Places disponibles: <span>${station.available_bike_stands}</span></p>
         `;
-      this.reservation(station);
-    });
+      //this.reservation(station);
+    })
+    ;
   }
 
   //---------- Block informations station -----------
   //---------------------------------------------------
-  reservation(station){
-    this.resa = station;
+  initReservationListener(){
     const buttonReserver = document.querySelector("#button-reservation").querySelector("button");
-    buttonReserver.addEventListener("click", ()=>{
-      if (this.resa.available_bikes > 0) {
+    buttonReserver.addEventListener("click", () => {
+      if (this.station.available_bikes > 0) {
         confirmation.style.display = "block";
         reserver.style.display = "none";
         this.confirmation();
-      } else {
+      }
+      else {
         confirmation.style.display = "none";
         reserver.style.display = "block";
         alert("aucun vélo n'est disponible dans cette station");
       }
-    });
-
+    })
   }
 
-  confirmation(){
+  confirmation() {
     const buttonConfirm = document.querySelector("#valider");
     const sectionTimer = document.querySelector("#timer");
     let intervalID = 0;
     let time;
-    buttonConfirm.addEventListener("click", () =>{
+    buttonConfirm.addEventListener("click", () => {
       sectionTimer.style.display = "block";
       sectionTimer.scrollIntoView();
-    });
+    })
+    ;
   }
 
 }
@@ -125,4 +130,5 @@ class App {
 addEventListener("load", () => {
   veloStation = new App();
   console.log(veloStation);
-});
+})
+;
